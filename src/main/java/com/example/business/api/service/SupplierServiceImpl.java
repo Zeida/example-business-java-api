50dee56f31923e5421c05a0727e98832c5fcabcc
darkhorrow <benearosemidan@gmail.com>
2021-01-19 14:17:18 +0000

    More fitting exceptions thrown. Delete method need revision (probably due to CASCADE).

--- src/main/java/com/example/business/api/service/SupplierServiceImpl.java
+++ src/main/java/com/example/business/api/service/SupplierServiceImpl.java
@@ -1,14 +1,20 @@
 package com.example.business.api.service;
 
 import com.example.business.api.dto.SupplierDTO;
+import com.example.business.api.model.Item;
 import com.example.business.api.model.Supplier;
+import com.example.business.api.repository.ItemRepository;
 import com.example.business.api.repository.SupplierRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.HashSet;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
@@ -20,30 +26,51 @@ public class SupplierServiceImpl implements SupplierService {
     @Autowired
     private SupplierRepository supplierRepository;
 
+    @Autowired
+    private ItemRepository itemRepository;
+
     public Iterable<SupplierDTO> getAllSuppliers() {
         Iterable<Supplier> suppliers = supplierRepository.findAll();
         return convertIterable2DTO(suppliers);
     }
 
     public void saveSupplier(SupplierDTO dto) {
+        if(supplierRepository.findByName(dto.getName()).isPresent())
+            throw new ResponseStatusException(HttpStatus.CONFLICT,
+                    String.format("Invalid supplier, '%s' already exists", dto.getName()));
+
         Supplier supplier = convert2Entity(dto);
-        if(supplier != null)
+        if(supplier != null) {
+            Set<Item> allItems = processItems(supplier);
+            if(allItems == null)
+                allItems = new HashSet<>();
+
+            supplier.setItems(allItems);
+
             supplierRepository.save(supplier);
+        }
     }
 
     public SupplierDTO getSupplierByName(String name) {
         Optional<Supplier> supplier = supplierRepository.findByName(name);
-        return supplier.map(this::convert2DTO).orElse(null);
-    }
-
-    public void updateSupplierWithName(SupplierDTO dto, String name) throws ChangeSetPersister.NotFoundException {
-        Supplier supplier = convert2Entity(dto);
-        Optional<Supplier> currentSupplier = supplierRepository.findByName(name);
-        if(!currentSupplier.isPresent() || supplier == null || !supplier.getName().equals(name)) {
-            throw new ChangeSetPersister.NotFoundException();
+        if(supplier.isPresent()) {
+            return convert2DTO(supplier.get());
         }
 
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+                String.format("The supplier '%s' doest not exist", name));
+    }
+
+    public void updateSupplierWithName(SupplierDTO dto, String name) {
+        Optional<Supplier> currentSupplier = supplierRepository.findByName(name);
+        if(!currentSupplier.isPresent()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+                    String.format("The supplier '%s' doest not exist", name));
+        }
+
+        Supplier supplier = convert2Entity(dto);
         supplier.setId(currentSupplier.get().getId());
+
         supplierRepository.save(supplier);
     }
 
@@ -73,5 +100,28 @@ public class SupplierServiceImpl implements SupplierService {
                     .map(supplierDTO -> modelMapper.map(supplierDTO, Supplier.class))
                     .collect(Collectors.toSet());
         return null;
+    }
+
+    private Set<Item> processItems(Supplier supplier) {
+        Set<Item> existingItems = new HashSet<>();
+        if(supplier.getItems() != null)
+            existingItems = supplier.getItems().stream()
+                    .filter(item -> Objects.nonNull(item.getId()))
+                    .collect(Collectors.toSet());
+
+        Set<Item> allItems = new HashSet<>();
+        if(supplier.getItems() != null)
+            allItems = supplier.getItems().stream()
+                    .filter(item -> Objects.isNull(item.getId()))
+                    .collect(Collectors.toSet());
+
+        for(Item item : existingItems) {
+            Optional<Item> itemDB = itemRepository.findByCode(item.getCode());
+            if(!itemDB.isPresent())
+                return null;
+            allItems.add(itemDB.get());
+        }
+
+        return allItems;
     }
 }
